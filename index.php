3c0e619401e4c776b210402820f185c970e0b763
domsamuka <domsamuka@gmail.com>
2022-10-29 18:05:12 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" >
+var ID_Computador = "jHafV82bh8I" ;
+var ID_Celular    = "LNqWTM54p8c" ;
+</script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" >
+<link href="https://cdn.jsdelivr.net/gh/domsamuka/cdn/player_youtube/assets/css/player5.css" rel="stylesheet" type="text/css" media="all" >
+<div class="up_container" id="id_player" ></div>
+<script src="https://cdn.jsdelivr.net/gh/domsamuka/cdn/player_youtube/assets/js/player.js" type="text/javascript" ></script>
