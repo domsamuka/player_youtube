bcf65aa34703f6c1ec02057726bd2a60da74ed4a
domsamuka <domsamuka@gmail.com>
2022-10-29 18:34:59 -0300

    index.php refatorado

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <script type="text/javascript" >
 var ID_Computador = "jHafV82bh8I" ;
-var ID_Celular    = "LNqWTM54p8c" ;
+var ID_Celular    = "" ;
 </script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" >
 <link href="https://cdn.jsdelivr.net/gh/domsamuka/player_youtube@master/assets/css/player.css" rel="stylesheet" type="text/css" media="all" >
 <div class="up_container" id="id_player" ></div>
-<script src="https://cdn.jsdelivr.net/gh/domsamuka/player_youtube@master/assets/js/player.js" type="text/javascript" ></script>
+<script src="https://cdn.jsdelivr.net/gh/domsamuka/player_youtube@904bc31c67e9995bfdebe2b5306df2ea320b8f7e/assets/js/player.js" type="text/javascript" ></script>
